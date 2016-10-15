1927fc6d275f2dcd09bf55e20e5d07743894246b
Juan Augusto <juan.augusto@ufrj.br>
2016-10-14 23:17:55 -0300

    Modifying trabalho 3

--- cg-trabalho-3/js/quadrado.js
+++ cg-trabalho-3/js/quadrado.js
@@ -6,14 +6,8 @@ var requestId;
 var delay = 100;
 
 var vColor;
-const black = vec4(0.0, 0.0, 0.0, 1.0);
-const red = vec4(1.0, 0.0, 0.0, 1.0);
 
-var Tx = 0.7, Ty = 0.0, Tz = 0.0;
 var u_Translation;
-var translacao_1, translacao_2;
-var matriz, resultado;
-var squareVerticesColorBuffer;
 
 var tecla_pressionada = false;
 
@@ -29,10 +23,6 @@ var sentido_rot_vargl;
 var vertice_giro;
 
 var vertices;
-
-//var bufferId, squareVerticesColorBuffer;
-
-//var vertexColorAttribute;
 
 $(document).ready(function () {
 
@@ -83,83 +73,58 @@ $(document).ready(function () {
     vertices = [
         vec2(0.7, 0.7),
         vec2(0.7, -0.7),
-
         vec2(-0.7, 0.7),
         vec2(-0.7, -0.7)
-
     ];
-
 
     var bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
     // Associate out shader variables with our data buffer
-
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-
     var colors = [
-        vec4(1.0,  1.0,  1.0,  1.0),    // white
-        vec4(1.0,  0.0,  0.0,  1.0),    // red
-        vec4(0.0,  1.0,  0.0,  1.0),    // green
-        vec4(0.0,  0.0,  1.0,  1.0)    // blue
+        vec4(1.0,  1.0,  1.0,  1.0),    // branco
+        vec4(1.0,  0.0,  0.0,  1.0),    // vermelho
+        vec4(0.0,  1.0,  0.0,  1.0),    // verde
+        vec4(0.0,  0.0,  1.0,  1.0)    // azul
     ];
 
     var squareVerticesColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 
-
+    // Associate out shader variables with our data buffer
     var vertexColorAttribute = gl.getAttribLocation(program, "aVertexColor");
     gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vertexColorAttribute);
 
-
-
     u_Translation = gl.getUniformLocation(program, "u_Translation");
     sentido_rot_vargl = gl.getUniformLocation(program, "sentido_rot");
 
-    //translacao_1 = gl.getUniformLocation(program, "translacao_1");
-    //translacao_2 = gl.getUniformLocation(program, "translacao_2");
-
     vColor = gl.getUniformLocation(program, "vColor");
     thetaLoc = gl.getUniformLocation(program, "theta");
-    //matriz = gl.getUniformLocation(program, "matriz");
 
     vertice_giro = vec4(0.7,0.7,0.0,0.0);
     sentido_rot = 1.0;
 
-
-
-    //gl.uniform4f(vColor, flatten(red));
-    //squareVerticesColorBuffer = gl.createBuffer();
-    //gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
-    //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
     render();
-
-
-
 
 });
 
 
 function render() {
-    //gl.clear( gl.COLOR_BUFFER_BIT |  gl.DEPTH_BUFFER_BIT);
+
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     theta += 5;
-    //if(theta>2*Math.PI){
-        //theta = 0.0;
-    //}
+
     gl.uniform1f(thetaLoc, theta);
     gl.uniform1f(sentido_rot_vargl, sentido_rot);
     gl.uniform4fv(u_Translation, vertice_giro);
-
-
-    //gl.uniform4f(translacao_1, 0.5, 0.0, 0.0, 0.0); //x=1 y=0 rotacionar sobre esse ponto
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
@@ -187,7 +152,6 @@ function render() {
 
                 requestId = requestAnimFrame(render);
 
-               
             }else{
                 if(theta==360){
                     theta = 0.0;
